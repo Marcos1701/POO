e9f1de5f4f6f3c2dfd168b3578151b5203102bf3
Marcos1701 <103659504+Marcos1701@users.noreply.github.com>
2022-08-10 20:30:43 -0300

    arrumando algumas coisinhas..
    nd de mais..

--- revisao/Questao_8.js
+++ revisao/Questao_8.js
@@ -1,4 +1,5 @@
-import { input } from './arq_auxiliar.js'
+import prompt from 'prompt-sync'
+const input = prompt()
 
 /*8. Escreva um programa que leia um texto pelo teclado 
 e remova todas as suas vogais
@@ -7,51 +8,34 @@ acentuadas por não acentuadas. Exiba a string resultante.*/
 function main() {
 
     console.log('Digite o trecho/frase a seguir: ')
-    const texto_entrada = input('- ')
+    const texto_entrada = input('=> ')
 
-    const string_retorno = remover_vogais_acentuadas(texto_entrada)
+    const string_retorno = remover_vogais(texto_entrada)
 
     console.log(string_retorno)
 }
 
-function remover_vogais_acentuadas(texto) {
-    const vogais_acentuadas = ('áéíóúãõâêîôûÁÉÍÓÚÂÊÎÔÛÃÕàèìòùÀÈÌÒÙ').split('')
-    const texto_retorno = texto.split('')
+function remover_vogais(texto) {
 
-    for (let i = 0; i < texto_retorno.length; i++) {
+    let texto_retorno = ''
+    for (let i = 0; i < texto.length; i++) {
 
-        for (let k = 0; k < vogais_acentuadas.length; k++) {
-            if (texto_retorno[i] === vogais_acentuadas[k]) {
-                texto_retorno[i] = substituir_vogal(texto_retorno[i], k)
-            }
+        if (conferir_caractere(texto[i])) {
+            texto_retorno += texto[i]
         }
     }
     return texto_retorno
 }
 
-function substituir_vogal(indice) {
+function conferir_caractere(caractere) {
+    const vogais_acentuadas = ('áéíóúãõâêîôûÁÉÍÓÚÂÊÎÔÛÃÕàèìòùÀÈÌÒÙ').split('')
 
-    if (indice == 0 || indice == 5 || indice == 7 || indice == 24) {
-        return 'a'
-    } else if (indice == 1 || indice == 8 || indice == 25) {
-        return 'e'
-    } else if (indice == 2 || indice == 9 || indice == 26) {
-        return 'i'
-    } else if (indice == 3 || indice == 6 || indice == 10 || indice == 27) {
-        return 'o'
-    } else if (indice == 4 || indice == 11 || indice == 28) {
-        return 'u'
-    } else if (indice == 12 || indice == 17 || indice == 22 || indice == 29) {
-        return 'A'
-    } else if (indice == 13 || indice == 18 || indice == 30) {
-        return 'E'
-    } else if (indice == 14 || indice == 19 || indice == 31) {
-        return 'I'
-    } else if (indice == 15 || indice == 20 || indice == 23 || indice == 32) {
-        return 'O'
-    } else if (indice == 16 || indice == 21 || indice == 33) {
-        return 'U'
+    for (let i = 0; i < vogais_acentuadas.length; i++) {
+        if (caractere == vogais_acentuadas[i]) {
+            return false
+        }
     }
+    return true
 }
 
 main()
