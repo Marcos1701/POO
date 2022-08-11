c39fb86798913a80224eedcb9d4c4c4ca3c5a366
Marcos1701 <103659504+Marcos1701@users.noreply.github.com>
2022-08-11 19:35:57 -0300

    Update Questao_9.js

--- revisao/Questao_9.js
+++ revisao/Questao_9.js
@@ -9,18 +9,20 @@ function main() {
     console.log('Digite a taxa de juros em %: ')
     const juros = (Number(input('=> '))) / 100
 
-    const vetor_valores = calcular_retorno(valor_inicial, juros)
+    const vetor_valores = (calcular_retorno(valor_inicial, juros)).join(' - ')
     console.log(vetor_valores)
 }
 
 function calcular_retorno(valor_inicial, juros) {
 
     const vetor_valores = new Array(12)
+    let valor_atual = valor_inicial
 
     vetor_valores[0] = valor_inicial
 
     for (let i = 1; i < vetor_valores.length; i++) {
-        vetor_valores[i] = ((vetor_valores[i - 1] * juros) + vetor_valores[i - 1]).toFixed(2)
+        valor_atual = ((vetor_valores[i - 1] * juros) + valor_atual)
+        vetor_valores[i] = (valor_atual).toFixed(2)
     }
     return vetor_valores
 }
