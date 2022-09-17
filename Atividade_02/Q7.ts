aa6f5c0fc62280cdab312922b125d8cd370e24f5
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-16 22:47:19 -0300

    Create Q7.ts

---
+++ Atividade_02/Q7.ts
@@ -0,0 +1,22 @@
+/*
+7. Reescreva o exemplo abaixo, mantendo a quebra de linhas usando template
+strings e os valores Ely, 120.56 e TypeScript venham de variáveis declaradas
+separadamente e “interpoladas” na string:
+Ely
+My payment time is 120.56
+and
+my preffered language is TypeScript
+*/
+
+```
+const nome: String = "Ely"
+const payment_time: number = 120.56
+const language: String = "TypeScript"
+
+const retorno: String = `${nome}
+My payment time is ${payment_time}
+and
+my preffered language is ${language}`
+
+console.log(retorno)
+```
