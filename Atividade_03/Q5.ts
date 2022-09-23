46ffa471a3763c096874bb2e17233f5542eb7437
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-09-22 21:45:41 -0300

    Update Q5.ts

--- Atividade_03/Q5.ts
+++ Atividade_03/Q5.ts
@@ -7,6 +7,8 @@ function soma(x: number, y?: any): number {
 return x + y
 }
 
-console.log(soma(1,2))
+console.log(soma(1,2))  // saída : 3
 
-console.log(soma(1,'2'))
+console.log(soma(1,"2")) // saída : 12
+
+console.log(soma(1)) // saída : 1
