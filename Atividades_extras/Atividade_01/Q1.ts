88d8d0e4a7533987b5c7578d3104c1cb1467fcd0
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-01 16:33:33 -0300

    Create Q1.ts

---
+++ Atividades_extras/Atividade_01/Q1.ts
@@ -0,0 +1,27 @@
+class Empregado {
+    _salario: number = 1500
+    calcularSalario(): number {
+        return this._salario
+    }
+}
+    
+class Diarista extends Empregado {
+    calcularSalario(): number { 
+        return this._salario/30;  
+    }
+}
+    
+class Horista extends Diarista {
+        calcularSalario(): number { 
+            return (this._salario/30) / 24  
+        }
+}
+
+let emp: Empregado = new Empregado()
+console.log(`Salário empregado: R$ ${emp.calcularSalario().toFixed(3)}`);
+
+emp = new Diarista();
+console.log(`Salário Diarista: R$ ${emp.calcularSalario().toFixed(3)}`);
+
+emp = new Horista();
+console.log(`Salário Horista: R$ ${emp.calcularSalario().toFixed(3)}`);
