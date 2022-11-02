bdfb1edb1aef020ef6f6dbd83ce7cdde681e4b82
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-02 14:06:16 -0300

    Add files via upload

---
+++ Atividades_extras/Atividade_02/Banco_testes/Banco.ts
@@ -0,0 +1,237 @@
+import { isReturnStatement } from "typescript"
+
+class Conta {
+
+    private _numero: string
+    private _saldo: number
+    constructor(numero: string, saldo: number) {
+        this._numero = numero
+        if (this.saldo < 0) { throw new Error("Saldo inválido!!!!") }
+        this._saldo = saldo
+    }
+
+    depositar(valor: number): void {
+        if (valor < 0) {
+            throw new Error("Erro, insira um valor positivo para deposito..")
+        }
+        this._saldo += valor
+    }
+
+    sacar(valor: number): void {
+
+        if (this._saldo < valor || valor < 0) {
+            throw new Error("Erro, insira um valor válido para o saque..")
+        }
+        this._saldo -= valor
+    }
+
+    get saldo(): number {
+        return this._saldo
+    }
+
+    get numero(): string {
+        return this._numero
+    }
+
+
+    transferir(conta: Conta, valor: number): void {
+        this.sacar(valor)
+        conta.depositar(valor)
+
+    }
+
+    informacoes_da_conta(): string {
+        const retorno: string = `
+----- Informações da conta -----
+Número da conta: ${this._numero}
+saldo atual: ${this._saldo.toFixed(3)}
+`
+        return retorno
+    }
+
+    private Trapamento_erros(msg: string): void {
+        return console.log(msg)
+    }
+}
+
+class Poupanca extends Conta {
+    private _taxaJuros: number;
+    constructor(numero: string, saldo: number, taxaJuros: number) {
+        super(numero, saldo)
+        this._taxaJuros = taxaJuros;
+    }
+
+    render_juros(): void {
+        super.depositar(this.saldo + (this.saldo * this._taxaJuros / 100));
+    }
+
+    informacoes_da_conta(): string {
+        let info = super.informacoes_da_conta();
+        info += `Taxa de Juros: ${this.taxaJuros}%
+Tipo da Conta: Conta Poupaça
+`
+        return info
+    }
+
+    get taxaJuros(): number {
+        return this._taxaJuros;
+    }
+
+}
+
+class ContaImposto extends Conta {
+    private _taxaImposto: number;
+    constructor(numero: string, saldo: number, taxaImposto: number) {
+        super(numero, saldo)
+        this._taxaImposto = taxaImposto;
+    }
+
+    sacar(valor: number): void {
+        const valor_saque = valor + (valor * this.taxaImposto / 100);
+        super.sacar(valor_saque);
+    }
+
+    informacoes_da_conta(): string {
+        let info = super.informacoes_da_conta();
+        info += `Taxa de Imposto: ${this.taxaImposto}%
+Tipo da Conta: Conta Imposto
+`
+        return info
+    }
+
+    get taxaImposto(): number {
+        return this._taxaImposto;
+    }
+
+}
+
+
+class Banco {
+    private contas: Conta[] = []
+
+    inserir(conta: Conta): void {
+        if (this.consultar_conta(conta.numero)) {
+            throw new Error("Erro, já existe uma conta com esse número cadastrada!!!")
+        }
+        this.contas.push(conta)
+
+    }
+
+    private Trapamento_erros(msg: string): void {
+        return console.log(msg)
+    }
+
+    consultar_conta(numero: string): Conta {
+        let conta!: Conta
+
+        for (let i = 0; i < this.contas.length; i++) {
+            if (this.contas[i].numero == numero) {
+                conta = this.contas[i]
+                break
+            }
+        }
+
+        return conta
+    }
+
+    private consultar_index_conta(numero: string): number {
+        let index: number = -1
+
+        for (let i = 0; i < this.contas.length; i++) {
+            if (this.contas[i].numero == numero) {
+                index = i
+                break
+            }
+        }
+
+        return index
+    }
+
+    alterar_conta(conta: Conta): boolean {
+        let index: number = this.consultar_index_conta(conta.numero)
+
+        if (index != -1) {
+            this.contas[index] = conta
+            return true
+        }
+
+        return false
+    }
+
+    excluir_conta(conta: Conta): boolean {
+        let index: number = this.consultar_index_conta(conta.numero)
+
+        if (index != -1) {
+            for (let i = index; i < this.contas.length - 1; i++) {
+                this.contas[i] = this.contas[i + 1]
+            }
+            this.contas.pop()
+
+            return true
+
+        }
+
+        return false
+    }
+
+    depositar(numero: string, quantia: number): void {
+        let conta: Conta = this.consultar_conta(numero)
+
+        if (!conta) {
+            throw this.Trapamento_erros("Erro, Conta inexistente no Banco!!")
+        }
+        conta.depositar(quantia)
+    }
+
+    sacar(numero: string, quantia: number): void {
+        let conta: Conta = this.consultar_conta(numero)
+
+        if (!conta) {
+            throw new Error("Erro, Conta inexistente no Banco!!")
+        }
+        conta.sacar(quantia)
+    }
+
+    transferir(numero_conta_1: string, numero_conta_2: string, quantia: number): void {
+        let conta_manda: Conta = this.consultar_conta(numero_conta_1)
+        let conta_recebe: Conta = this.consultar_conta(numero_conta_2)
+
+        if (!conta_manda || !conta_recebe) {
+            throw new Error("Erro, uma das contas não pertence ou existe no banco!!")
+        }
+        conta_manda.transferir(conta_recebe, quantia)
+    }
+
+    renderJuros(numero: string): boolean {
+        const conta: Conta = this.consultar_conta(numero)
+
+        if (conta instanceof Poupanca) {
+            conta.render_juros();
+            return true
+        }
+        return false
+    }
+
+    qtd_contas(): number {
+        return this.contas.length
+    }
+
+    Soma_saldo(): number {
+        let valor: number = 0
+
+        for (let conta of this.contas) {
+            valor += conta.saldo
+        }
+
+        return valor
+    }
+
+    media_saldo(): number {
+        let media: number = (this.Soma_saldo()) / (this.qtd_contas())
+
+        return media
+    }
+
+}
+
+export { Conta, Banco, Poupanca, ContaImposto }
---
+++ Atividades_extras/Atividade_02/Banco_testes/app.ts
@@ -0,0 +1,205 @@
+import fs from 'fs'
+import prompt from 'prompt-sync'
+import { Conta, Banco, Poupanca, ContaImposto } from "./Banco.js"
+const input = prompt();
+
+
+function main() {
+    let b: Banco = new Banco();
+    let opcao: String = '';
+    do {
+        menu();
+        opcao = input("Opção: => ");
+
+        if (opcao == '1') {
+            inserir_Contas(b)
+        } else if (opcao == '2') {
+            consultar(b)
+        } else if (opcao == '3') {
+            sacar(b)
+        } else if (opcao == '4') {
+            depositar(b)
+        } else if (opcao == '5') {
+            renderJuros(b)
+        } else if (opcao == '6') {
+            excluir(b)
+        } else if (opcao == '7') {
+            tranfere(b)
+        } else if (opcao == '8') {
+            totaliza(b)
+        } else if (opcao == '9') {
+            inserirApartirdeArquivo(b)
+        } else if (opcao != '0') {
+            console.log("\nOpção inválida!!!")
+        }
+
+        input("Operação finalizada. Digite <enter>");
+    } while (opcao != "0");
+    console.log("\nAplicação encerrada");
+}
+
+function menuContas(): void {
+    console.log(`---- Criar Conta ---
+        1 - Conta
+        2 - Conta Poupança
+        3 - Conta Imposto`)
+}
+
+
+function inserir_Contas(b: Banco): void {
+
+    menuContas();
+    let op = Number(input("=> "));
+
+    while (op > 3 || op < 1) {
+        console.log('Opção inválida!!')
+        menuContas();
+        op = Number(input("=> "));
+    }
+
+    let numero: string = input("Digite o número da conta: ")
+    let saldo: number = Number(input("Digite o saldo da conta: "))
+
+    if (op == 1) {
+        let conta: Conta = new Conta(numero, saldo)
+        b.inserir(conta)
+    } else if (op == 2) {
+        const taxa_juros = Number(input("Digite a Taxa de Juros: "))
+        let conta_P: Poupanca = new Poupanca(numero, saldo, taxa_juros)
+        b.inserir(conta_P)
+    } else {
+        const taxa_Imposto = Number(input("Digite a Taxa de Imposto: "))
+        let conta_I: ContaImposto = new ContaImposto(numero, saldo, taxa_Imposto)
+        b.inserir(conta_I)
+    }
+
+}
+
+function menu(): void {
+    console.log('\nBem vindo\nDigite uma opção:');
+    console.log('1 - CriarConta 2 - ConsultarConta 3 - Sacar\n' +
+        '4 - Depositar  5 - renderJuros(Conta Poupança) 6 - Excluir\n' +
+        '7 - Transferir 8 – Totalizações\n' +
+        '9 - inserir contas apartir de arquivo de texto\n' +
+        '0 - Sair\n')
+}
+
+function inserirApartirdeArquivo(b: Banco) {
+
+    console.log("Observações: ")
+    console.log(`- Os dados do arquivos devem ser separados por ';'
+- 'C' indica uma conta normal, 'CI' indica uma conta imposto e 'P' indica uma conta poupança
+- Os dados devem estar organizados da seguinte maneira: 
+  > 'tipo da conta;numero;saldo;Taxa de juros(caso sendo poupança) ou Taxa de Imposto(caso conta imposto).'
+`)
+
+    const nomeArquivo: string = input("Digite o nome do arquivo com as contas: ")
+
+    const leitor_de_arquivo: string[] = fs.readFileSync(`./${nomeArquivo}`, 'utf-8').split('\n')
+
+    for (let i = 0; i < leitor_de_arquivo.length; i++) {
+        analisar_linha_arquivo(leitor_de_arquivo[i], b)
+    }
+}
+
+function analisar_linha_arquivo(linha: string, b: Banco): void {
+
+    const linha_frag: string[] = linha.split(';')
+
+    if (linha_frag[0] == "C") {
+        const conta_normal: Conta = new Conta(linha_frag[1], Number(linha_frag[2]))
+        b.inserir(conta_normal)
+    } else if (linha_frag[0] == "P") {
+        const conta_Poupanca: Poupanca = new Poupanca(linha_frag[1], Number(linha_frag[2]), Number(linha_frag[3]))
+        b.inserir(conta_Poupanca)
+    } else if (linha_frag[0] == "CI") {
+        const conta_Imposto: ContaImposto = new ContaImposto(linha_frag[1], Number(linha_frag[2]), Number(linha_frag[3]))
+        b.inserir(conta_Imposto)
+    } else {
+        console.log(`\nEm uma das linhas, o valor '${linha_frag[0]} foi inserido como um tipo de conta, mas esse valor é inválido!!!\n`)
+    }
+}
+
+
+function renderJuros(b: Banco): void {
+    let numero: string = input("Digite o número da conta: ")
+    if (b.renderJuros(numero)) {
+        console.log("Operação realizada com sucesso")
+    } else {
+        console.log("Erro, o número inserido não pertence a nenhuma conta Poupança")
+    }
+}
+
+
+function consultar(b: Banco): void {
+    console.log("\nConsultar conta\n")
+    let numero: string = input('Digite o número da conta: ')
+    let conta: Conta | Poupanca | ContaImposto = b.consultar_conta(numero)
+
+    if (conta) {
+        console.log(conta.informacoes_da_conta())
+    } else {
+        console.log("\nA conta não existe!!")
+    }
+    return
+}
+
+
+
+function sacar(b: Banco): void {
+
+    console.log("\nSacar\n")
+    let numero: string = input('Digite o número da conta: ')
+    let quantia: number = Number(input('Digite o Valor de saque: R$ '))
+
+    b.sacar(numero, quantia)
+    console.log("\nSaque Realizado com sucesso")
+
+}
+
+function depositar(b: Banco): void {
+
+    console.log("\nDepositar\n")
+    let numero: string = input('Digite o número da conta: ')
+    let quantia: number = Number(input('Digite o Valor de Deposito: R$ '))
+
+    b.depositar(numero, quantia)
+    console.log("\nDeposito realizado com sucesso!!")
+
+}
+
+function excluir(b: Banco): void {
+
+    console.log("\nExcluir\n")
+    let numero: string = input('Digite o número da conta: ')
+
+    if (b.excluir_conta(b.consultar_conta(numero))) {
+        console.log("\nConta excluída com sucesso!!!")
+    } else {
+        console.log("\nConta inexistente, confira o número digitado!!")
+    }
+
+}
+
+function tranfere(b: Banco): void {
+
+    console.log("\nTranferir\n")
+    let numero_manda: string = input('Digite o número da conta que irá realizar a tranferencia: ')
+    let numero_recebe: string = input('Digite o número da conta que irá receber a tranferencia: ')
+    let quantia: number = Number(input('Digite o Valor de Deposito: R$ '))
+
+    b.transferir(numero_manda, numero_recebe, quantia)
+    console.log("\nTranferencia Realizada com sucesso!!!")
+}
+
+function totaliza(b: Banco): void {
+
+    console.log(`
+ ----- Totalizações Banco -----
+Saldo Total: R$ ${b.Soma_saldo()}
+Total de contas: ${b.qtd_contas()} contas
+Média Saldo: R$ ${b.media_saldo()}
+`)
+}
+
+main()
