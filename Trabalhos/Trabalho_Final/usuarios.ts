e1188d436e14b8855bff6c27842751c659cdab25
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-11-30 09:57:49 -0300

    Update usuarios.ts

--- Trabalhos/Trabalho_Final/usuarios.ts
+++ Trabalhos/Trabalho_Final/usuarios.ts
@@ -1,20 +1,20 @@
 abstract class Usuario_base {
 
-    private _id: string;
+    private _id: number;
     private _nome: string;
-    private _email: string;
+    private _login: string;
     private _senha: string;
     private _idade: number;
 
-    constructor(id: string, nome: string, idade: number, email: string, senha: string) {
+    constructor(id: number, nome: string, idade: number, login: string, senha: string) {
         this._id = id;
         this._nome = nome;
         this._idade = idade;
-        this._email = email;
+        this._login = login;
         this._senha = senha;
     }
 
-    get id(): string {
+    get id(): number {
         return this._id;
     }
 
@@ -22,8 +22,8 @@ abstract class Usuario_base {
         return this._nome;
     }
 
-    get email(): string {
-        return this._email;
+    get login(): string {
+        return this._login
     }
 
     get senha(): string {
@@ -38,7 +38,7 @@ abstract class Usuario_base {
 
 
 interface IAutenticavel {
-    autenticar(email: string, senha: string): boolean;
+    autenticar(login: string, senha: string): boolean;
 }
 
 
@@ -46,11 +46,11 @@ class Usuario extends Usuario_base implements IAutenticavel {
 
     private Redes_sociais: RedeSocial[] = []
 
-    constructor(id: string, nome: string, idade: number, email: string, senha: string) {
-        super(id, nome, idade, email, senha);
+    constructor(id: number, nome: string, idade: number, login: string, senha: string) {
+        super(id, nome, idade, login, senha);
     }
 
-    consultar_rede_social(id: string): RedeSocial {
+    consultar_rede_social(id: number): RedeSocial {
         for (let rede of this.Redes_sociais) {
             if (rede.id == id) {
                 return rede
@@ -59,7 +59,7 @@ class Usuario extends Usuario_base implements IAutenticavel {
         throw new rede_social_inexistente("Erro, rede social inexistente!!!")
     }
 
-    private consultar_index_rede_social(id: string): number {
+    private consultar_index_rede_social(id: number): number {
         for (let i = 0; i < this.Redes_sociais.length; i++) {
             if (this.Redes_sociais[i].id == id) {
                 return i
@@ -73,7 +73,7 @@ class Usuario extends Usuario_base implements IAutenticavel {
         this.Redes_sociais.push(rede);
     }
 
-    excluir_rede_social(id_rede: string): void {
+    excluir_rede_social(id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede);
 
         for (let i = index; i < this.Redes_sociais.length; i++) {
@@ -82,24 +82,24 @@ class Usuario extends Usuario_base implements IAutenticavel {
         this.Redes_sociais.push()
     }
 
-    inserir_novo_post(post: Post, id_rede: string): void {
+    inserir_novo_post(post: Post, id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede);
         this.Redes_sociais[index].inserir_post(post);
     }
 
-    alterar_post(id_post: string, novo_txt: string, nova_legenda: string, id_rede: string): void {
+    alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede);
         this.Redes_sociais[index].alterar_post(id_post, novo_txt, nova_legenda);
     }
 
-    excluir_post(id_post: string, id_rede: string): void {
+    excluir_post(id_post: number, id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede)
         this.Redes_sociais[index].excluir_post(id_post);
     }
 
 
-    autenticar(email: string, senha: string): boolean {
-        if (email != this.email || senha != this.senha) {
+    autenticar(login: string, senha: string): boolean {
+        if (login != this.login || senha != this.senha) {
             throw new login_invalido("Erro, login inválido!!!")
         }
         return true;
