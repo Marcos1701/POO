18f18473fb71d4aea63bde701615b942e6300215
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-12-01 12:49:36 -0300

    main

--- Trabalhos/Trabalho_Final/repositorio.ts
+++ Trabalhos/Trabalho_Final/repositorio.ts
@@ -1,13 +1,9 @@
-interface Repositorio_Vetor {
-    Redes_sociais: RedeSocial[]
+interface Repositorio_Vetor<T> {
+    vetor: T[]
 
-    inserir_rede_social(rede: RedeSocial): void;
-    consultar_rede_social(id: number): RedeSocial;
-    excluir_rede_social(id_rede: number): void;
-    excluir_post(id_post: number, id_rede: number): void
-    alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void
-    inserir_novo_post(post: Post, id_rede: number): void
-
+    inserir(valor: T): void;
+    consultar(id: number): RedeSocial;
+    excluir(id: number): void;
 }
 
 interface Repositorio_encadeado<T> {
@@ -16,75 +12,91 @@ interface Repositorio_encadeado<T> {
     inserir(valor: T): void;
     consultar(id: number): RedeSocial;
     excluir(id: number): void;
-    excluir(id_post: number, id_rede: number): void
-    alterar(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void
-    inserir(post: Post, id_rede: number): void
-
 }
 
-class Repo_vetor implements Repositorio_Vetor {
+class Repo_vetor<T> implements Repositorio_Vetor<RedeSocial>{
 
-    Redes_sociais: RedeSocial[] = []
+    vetor: T[] = []
 
-    consultar_rede_social(id: number): RedeSocial {
-        for (let rede of this.Redes_sociais) {
-            if (rede.id == id) {
-                return rede
+    consultar(id: number): T {
+        for (let valor of this.vetor) {
+            if (valor.id == id) {
+                return valor
             }
         }
         throw new rede_social_inexistente("Erro, rede social inexistente!!!")
     }
 
-    private consultar_index_rede_social(id: number): number {
-        for (let i = 0; i < this.Redes_sociais.length; i++) {
-            if (this.Redes_sociais[i].id == id) {
+    private consultar_index_valor(id: number): number {
+        for (let i = 0; i < this.vetor.length; i++) {
+            if (this.vetor[i].id == id) {
                 return i
             }
         }
         throw new rede_social_inexistente("Erro, rede social inexistente!!!")
     }
 
-    inserir_rede_social(rede: RedeSocial): void {
-        this.consultar_rede_social(rede.id)
-        this.Redes_sociais.push(rede);
+    inserir(valor: T): void {
+        this.consultar(valor.id)
+        this.Redes_sociais.push(valor);
     }
 
-    excluir_rede_social(id_rede: number): void {
+    excluir(id: number): void {
         const index: number = this.consultar_index_rede_social(id_rede);
 
-        for (let i = index; i < this.Redes_sociais.length; i++) {
-            this.Redes_sociais[i] = this.Redes_sociais[i + 1]
+        for (let i = index; i < this.vetor.length; i++) {
+            this.vetor[i] = this.vetor[i + 1]
         }
-        this.Redes_sociais.push()
+        this.vetor.push()
     }
 
-    inserir_novo_post(post: Post, id_rede: number): void {
-        const index: number = this.consultar_index_rede_social(id_rede);
-        this.Redes_sociais[index].inserir_post(post);
+}
+
+class Repo_vetor_to_usuario extends Repo_vetor {
+    inserir_novo_post(post: Post, id: number): void {
+        const index: number = this.consultar_index_rede_social(id);
+        this.vetor[index].inserir_post(post);
     }
 
     alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede);
-        this.Redes_sociais[index].alterar_post(id_post, novo_txt, nova_legenda);
+        this.vetor[index].alterar_post(id_post, novo_txt, nova_legenda);
     }
 
     excluir_post(id_post: number, id_rede: number): void {
         const index: number = this.consultar_index_rede_social(id_rede)
-        this.Redes_sociais[index].excluir_post(id_post);
+        this.vetor[index].excluir_post(id_post);
+    }
+}
+
+class Repo_vetor_to_Rede_Social extends Repo_vetor {
+    inserir_novo_post(post: Post, id: number): void {
+        const index: number = this.consultar_index_rede_social(id);
+        this.vetor[index].inserir_post(post);
+    }
+
+    alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void {
+        const index: number = this.consultar_index_rede_social(id_rede);
+        this.vetor[index].alterar_post(id_post, novo_txt, nova_legenda);
+    }
+
+    excluir_post(id_post: number, id_rede: number): void {
+        const index: number = this.consultar_index_rede_social(id_rede)
+        this.vetor[index].excluir_post(id_post);
     }
 }
 
 
 class No<T>{
-    private _Valor: T
+    private _valor: T
     proximo: No<T> | null = null
     anterior: No<T> | null = null;
 
     constructor(valor: T) {
-        this._Valor = valor
+        this._valor = valor
     }
-    get Valor(): T {
-        return this._Valor
+    get valor(): T {
+        return this._valor
     }
 }
 
@@ -92,67 +104,65 @@ class Repo_encadeado implements Repositorio_encadeado<RedeSocial> {
     inicio: No<RedeSocial> | null = null
     contador: number = 0
 
-    consultar_rede_social(id: number): No<RedeSocial> {
+    consultar(id: number): No<RedeSocial> {
 
-        let aux: No<RedeSocial> | null = this.inicio
+        let aux: No<RedeSocial> | null = this.inicio;
         while (aux) {
-            if (aux.Valor.id == id) {
-                return aux
+            if (aux.valor.id == id) {
+                return aux;
             }
-            aux = aux.proximo
+            aux = aux.proximo;
         }
-        throw new rede_social_inexistente("Erro, rede social inexistente!!!")
+        throw new rede_social_inexistente("Erro, rede social inexistente!!!");
     }
 
-    // private consultar_index_rede_social(id: number): number {
-
-    //     throw new rede_social_inexistente("Erro, rede social inexistente!!!")
-    // }
-
-    inserir_rede_social(rede: RedeSocial): void {
+    inserir(valor: RedeSocial): void {
         try {
-            this.consultar_rede_social(rede.id)
+            this.consultar_rede_social(rede.id);
         } catch (e: any) {
             if (e instanceof rede_social_inexistente) {
                 if (this.inicio) {
-                    let aux: No<RedeSocial> | null = this.inicio
+                    let aux: No<RedeSocial> | null = this.inicio;
                     while (aux) {
                         aux = aux.proximo;
                     }
                 } else {
-                    this.inicio = new No<RedeSocial>(rede)
+                    this.inicio = new No<RedeSocial>(rede);
                 }
             } else {
-                throw new rede_social_ja_existente("Erro, a rede social inserida já foi adicionada anteriormente!!!")
+                throw new rede_social_ja_existente("Erro, a rede social inserida já foi adicionada anteriormente!!!");
             }
         }
 
     }
 
-    excluir_rede_social(id_rede: number): void {
+    excluir(id_rede: number): void {
 
-        if (this.inicio) { throw new rede_social_inexistente("Rede Social não encontrada!!!") }
-        let aux: No<RedeSocial> = this.consultar_rede_social(id_rede)
+        if (this.inicio) { throw new rede_social_inexistente("Rede Social não encontrada!!!") };
+        let aux: No<RedeSocial> = this.consultar_rede_social(id_rede);
         if (aux == this.inicio) {
-            this.inicio = this.inicio.proximo
+            this.inicio = this.inicio.proximo;
         } else if (aux.anterior) {
-            aux.anterior.proximo = aux.proximo
+            aux.anterior.proximo = aux.proximo;
             if (aux.proximo) {
-                aux.proximo.anterior = aux.anterior
+                aux.proximo.anterior = aux.anterior;
             }
         }
     }
 
     inserir_novo_post(post: Post, id_rede: number): void {
-
+        const rede: RedeSocial = this.consultar(id_rede);
+        rete.inserir_post(post);
     }
 
     alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void {
-        //this.Redes_sociais[index].alterar_post(id_post, novo_txt, nova_legenda);
+        const rede: RedeSocial = this.consultar(id_rede);
+        rede.alterar_post(id_post, novo_txt, nova_legenda);
     }
 
     excluir_post(id_post: number, id_rede: number): void {
-        //this.Redes_sociais[index].excluir_post(id_post);
+        const rede: RedeSocial = this.consultar(id_rede)
+        rede.excluir_post(id_post);
     }
 }
 
