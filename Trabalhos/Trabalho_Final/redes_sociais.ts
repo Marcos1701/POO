234b0b5952974c61e043bc39f50e8da2234bee13
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-12-10 14:55:29 -0300

    Update redes_sociais.ts

--- Trabalhos/Trabalho_Final/redes_sociais.ts
+++ Trabalhos/Trabalho_Final/redes_sociais.ts
@@ -1,16 +1,20 @@
+import { IAutenticavel, Usuario } from "./usuarios";
+import { post_inexistente, ValorInvalido, Valor_inexistente } from "./trata_erros";
+import { repo_post } from './repositorio'
+export { RedeSocial, Post }
+
 class Post {
     private _id: number
     private _texto: string
-    private _autor: IAutenticavel
+    private _autor: Usuario
     private _curtidas: number
     private _legenda: string
     private _data: Date
-    private Usuarios_que_curtiram: Usuario_V[] = []
 
     constructor(
         id: number,
         texto: string,
-        autor: IAutenticavel,
+        autor: Usuario,
         legenda: string,
         data: Date, curtidas: number = 0
     ) {
@@ -29,18 +33,22 @@ class Post {
             throw new ValorInvalido("Erro, um dos valores inseridos é inválido!!");
         }
     }
-    curtir(usuario: Usuario_V): void {
-        if (usuario instanceof Usuario_V) {
-            this._curtidas++;
-            this.Usuarios_que_curtiram.push(usuario)
-        }
+
+    curtir(): void {
+        this._curtidas++;
 
     }
 
-    exibir_usuarios_que_curtiram(): void {
-        for (let usuario of this.Usuarios_que_curtiram) {
-            console.log(`Nome: ${usuario.nome}\nidade: ${usuario.idade}`)
-        }
+    descurtir(): void {
+        this._curtidas--;
+    }
+
+    visualizar(): void {
+        console.log(`ID: ${this._id}`)
+        console.log(`Texto: ${this._texto}`)
+        console.log(`Legenda: ${this.legenda}`)
+        console.log(`Data: ${this._data.getDate()}/${this._data.getMonth() + 1}/${this._data.getFullYear()}`)
+        console.log(`Autor: ${this._autor.nome}`)
     }
 
     get id(): number {
@@ -68,10 +76,11 @@ class Post {
 
 
 class RedeSocial {
-    private Posts: Post[] = []
+    private Armazena_dados: repo_post = new repo_post()
     constructor(
         private _id: number,
         private _nome: string,
+        private _id_post: number = 1
     ) { }
 
     get nome(): string {
@@ -82,56 +91,62 @@ class RedeSocial {
         return this._id
     }
 
-    consultar_post(id: number): Post {
-        for (let post of this.Posts) {
-            if (post.id == id) {
-                return post
-            }
-        }
-        throw new post_inexistente("Post não encontrado")
+    get id_post(): number {
+        return this._id_post
     }
 
-    private consultar_index_post(id: number): number {
-        for (let i = 0; i < this.Posts.length; i++) {
-            if (this.Posts[i].id == id) {
-                return i
-            }
+    ValidarValor(valor: number): void {
+        if (isNaN(valor) || valor < 0) {
+            throw new ValorInvalido("Erro, um dos valores inseridos é inválido!!");
         }
-        throw new post_inexistente("Post não encontrado")
     }
+
+    consultar_post(id: number): Post {
+        this.ValidarValor(id)
+        let post!: Post
+        try {
+            post = this.Armazena_dados.consultar(id).Valor
+        } catch (e: any) {
+            if (e instanceof Valor_inexistente) {
+                throw new post_inexistente("Erro, o post não foi encontrado!!")
+            }
+
+        }
+        return post
+    }
+
 
     inserir_post(post: Post): void {
-        try {
-            this.consultar_post(post.id);
-        } catch (e: any) {
-            if (!(e instanceof post_inexistente)) {
-                throw new post_ja_criado("Erro, esse post já foi inserido!!")
-            }
-            this.Posts.push(post)
-        }
+        this.Armazena_dados.inserir(post)
+        this._id_post++;
     }
 
-    alterar_post(id: number, texto: string, legenda: string): void {
-        const index = this.consultar_index_post(id)
-        const post: Post = new Post(id, texto, this.Posts[index].autor, legenda, new Date(), this.Posts[index].curtidas)
-        this.Posts[index] = post
+    alterar_post(novo_post: Post): void {
+        this.ValidarValor(novo_post.id)
+        this.Armazena_dados.alterar(novo_post)
     }
 
     excluir_post(id: number): void {
-        const index = this.consultar_index_post(id)
-        for (let i = index; i < this.Posts.length; i++) {
-            this.Posts[i] = this.Posts[i + 1]
-        }
-        this.Posts.push()
+        this.ValidarValor(id)
+        this.Armazena_dados.excluir(id)
     }
 
-    curtir_post(id: number, usuario: Usuario_V) {
-        const index = this.consultar_index_post(id)
-        this.Posts[index].curtir(usuario);
+    curtir_post(id: number): void {
+        this.ValidarValor(id)
+        this.Armazena_dados.curtir(id)
     }
 
+    descurtir_post(id: number): void {
+        this.ValidarValor(id)
+        this.Armazena_dados.descurtir(id)
+    }
+
+    visualizar_post(id: number): void {
+        this.ValidarValor(id)
+        this.Armazena_dados.visualizar(id)
+    }
+
+    visualizar_todos_os_posts(): void {
+        this.Armazena_dados.visualizar_todos()
+    }
 }
-
-import { IAutenticavel, Usuario_V } from "./usuarios";
-import { post_inexistente, post_ja_criado, ValorInvalido } from "./trata_erros";
-export { RedeSocial, Post }
