676e7345ef7ff2a609c532f2a0a17b644574562e
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-12-10 14:55:58 -0300

    Update usuarios.ts

--- Trabalhos/Trabalho_Final/usuarios.ts
+++ Trabalhos/Trabalho_Final/usuarios.ts
@@ -1,15 +1,18 @@
+import { login_invalido } from "./trata_erros"
+import { RedeSocial, Post } from "./redes_sociais"
+import { No, repo_rede_social } from './repositorio'
+export { Usuario, IAutenticavel }
+
 abstract class Usuario_base {
 
     private _id: number;
     private _nome: string;
     private _login: string;
     private _senha: string;
-    private _idade: number;
 
-    constructor(id: number, nome: string, idade: number, login: string, senha: string) {
+    constructor(id: number, nome: string, login: string, senha: string) {
         this._id = id;
         this._nome = nome;
-        this._idade = idade;
         this._login = login;
         this._senha = senha;
     }
@@ -30,9 +33,6 @@ abstract class Usuario_base {
         return this._senha;
     }
 
-    get idade(): number {
-        return this._idade
-    }
 }
 
 
@@ -44,40 +44,62 @@ interface IAutenticavel {
 
 
 
-class Usuario_V extends Usuario_base implements IAutenticavel{
+class Usuario extends Usuario_base implements IAutenticavel {
 
-    private Repositorio_dados: Repositorio_redes
+    private Repositorio_dados: repo_rede_social = new repo_rede_social()
 
-    constructor(id: number, nome: string, idade: number, login: string, senha: string, Repositorio: Repositorio_redes) {
-        super(id, nome, idade, login, senha);
-        this.Repositorio_dados = Repositorio
+    constructor(id: number, nome: string, login: string, senha: string) {
+        super(id, nome, login, senha);
     }
 
     consultar_rede_social(id: number): RedeSocial {
-       return this.Repositorio_dados.consultar_rede_social(id)
+        const no: No<RedeSocial> = this.Repositorio_dados.consultar(id)
+        const rede_social = no.Valor
+        return rede_social
     }
 
     inserir_rede_social(rede: RedeSocial): void {
-         this.Repositorio_dados.inserir_rede_social(rede)
+        this.Repositorio_dados.inserir(rede)
     }
 
     excluir_rede_social(id_rede: number): void {
-        this.Repositorio_dados.excluir_rede_social(id_rede)
+        this.Repositorio_dados.excluir(id_rede)
+    }
+
+    exibir_redes_sociais(): void {
+        let aux: No<RedeSocial> | null = this.Repositorio_dados.inicio
+
+        console.log('Redes sociais cadastradas: \n')
+        console.log('ID - Nome da Rede Social')
+
+        while (aux) {
+            console.log(`${aux.Valor.id} - ${aux.Valor.nome}`)
+            aux = aux.proximo
+        }
+        console.log("\n")
     }
 
     inserir_novo_post(post: Post, id_rede: number): void {
         this.Repositorio_dados.inserir_novo_post(post, id_rede)
     }
 
-    alterar_post(id_post: number, novo_txt: string, nova_legenda: string, id_rede: number): void {
-       this.Repositorio_dados.alterar_post(id_post, novo_txt, nova_legenda, id_rede)
+    curtir_post(id_rede: number, id_post: number): void {
+        const rede: RedeSocial = this.consultar_rede_social(id_rede)
+        rede.curtir_post(id_post)
+    }
+
+    descurtir_post(id_rede: number, id_post: number): void {
+        const rede: RedeSocial = this.consultar_rede_social(id_rede)
+        rede.descurtir_post(id_post)
+    }
+
+    alterar_post(novo_post: Post, id_rede: number): void {
+        this.Repositorio_dados.alterar_post(id_rede, novo_post)
     }
 
     excluir_post(id_post: number, id_rede: number): void {
         this.Repositorio_dados.excluir_post(id_post, id_rede);
-        
     }
-
 
     autenticar(login: string, senha: string): boolean {
         if (login != this.login || senha != this.senha) {
@@ -86,10 +108,3 @@ class Usuario_V extends Usuario_base implements IAutenticavel{
         return true;
     }
 }
-
-
-
-import { login_invalido, rede_social_inexistente } from "./trata_erros"
-import { RedeSocial, Post } from "./redes_sociais"
-import { Repositorio_redes } from './repositorio'
-export { Usuario_V, IAutenticavel }
