75217fd313d6c2b9afb397da7a7d8302d00f57a8
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-12-12 22:26:24 -0300

    Update usuarios.ts

--- Trabalhos/Trabalho_Final/usuarios.ts
+++ Trabalhos/Trabalho_Final/usuarios.ts
@@ -46,41 +46,50 @@ interface IAutenticavel {
 
 class Usuario extends Usuario_base implements IAutenticavel {
 
-    private Repositorio_dados: repo_rede_social = new repo_rede_social()
+    private Repositorio_de_redes_sociais: repo_rede_social = new repo_rede_social()
 
     constructor(id: number, nome: string, login: string, senha: string) {
         super(id, nome, login, senha);
     }
 
     consultar_rede_social(id: number): RedeSocial {
-        const no: No<RedeSocial> = this.Repositorio_dados.consultar(id)
+        const no: No<RedeSocial> = this.Repositorio_de_redes_sociais.consultar(id)
         const rede_social = no.Valor
         return rede_social
     }
 
+    ids_redes(): number[] {
+        return this.Repositorio_de_redes_sociais.ids_ja_utilizados()
+    }
+
     inserir_rede_social(rede: RedeSocial): void {
-        this.Repositorio_dados.inserir(rede)
+        this.Repositorio_de_redes_sociais.inserir(rede)
     }
 
     excluir_rede_social(id_rede: number): void {
-        this.Repositorio_dados.excluir(id_rede)
+        this.Repositorio_de_redes_sociais.excluir(id_rede)
     }
 
     exibir_redes_sociais(): void {
-        let aux: No<RedeSocial> | null = this.Repositorio_dados.inicio
+        let aux: No<RedeSocial> | null = this.Repositorio_de_redes_sociais.inicio
 
         console.log('Redes sociais cadastradas: \n')
-        console.log('ID - Nome da Rede Social')
 
         while (aux) {
-            console.log(`${aux.Valor.id} - ${aux.Valor.nome}`)
+            console.log(`ID: ${aux.Valor.id}`)
+            console.log(`NOME: ${aux.Valor.nome}`)
+            console.log(`URL: ${aux.Valor.url}\n`)
             aux = aux.proximo
         }
         console.log("\n")
     }
 
+    qtd_redes(): number {
+        return this.Repositorio_de_redes_sociais.qtd
+    }
+
     inserir_novo_post(post: Post, id_rede: number): void {
-        this.Repositorio_dados.inserir_novo_post(post, id_rede)
+        this.Repositorio_de_redes_sociais.inserir_novo_post(post, id_rede)
     }
 
     curtir_post(id_rede: number, id_post: number): void {
@@ -88,17 +97,13 @@ class Usuario extends Usuario_base implements IAutenticavel {
         rede.curtir_post(id_post)
     }
 
-    descurtir_post(id_rede: number, id_post: number): void {
-        const rede: RedeSocial = this.consultar_rede_social(id_rede)
-        rede.descurtir_post(id_post)
-    }
 
     alterar_post(novo_post: Post, id_rede: number): void {
-        this.Repositorio_dados.alterar_post(id_rede, novo_post)
+        this.Repositorio_de_redes_sociais.alterar_post(id_rede, novo_post)
     }
 
     excluir_post(id_post: number, id_rede: number): void {
-        this.Repositorio_dados.excluir_post(id_post, id_rede);
+        this.Repositorio_de_redes_sociais.excluir_post(id_post, id_rede);
     }
 
     autenticar(login: string, senha: string): boolean {
