903f4ae3d2356e1b47d515cdd8f573bab34978ef
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2022-12-12 22:27:03 -0300

    Update redes_sociais.ts

--- Trabalhos/Trabalho_Final/redes_sociais.ts
+++ Trabalhos/Trabalho_Final/redes_sociais.ts
@@ -1,6 +1,6 @@
 import { IAutenticavel, Usuario } from "./usuarios";
-import { post_inexistente, ValorInvalido, Valor_inexistente } from "./trata_erros";
-import { repo_post } from './repositorio'
+import { post_inexistente, usuario_inexistente, ValorInvalido, Valor_inexistente } from "./trata_erros";
+import { repo_post, No } from './repositorio'
 export { RedeSocial, Post }
 
 class Post {
@@ -39,16 +39,13 @@ class Post {
 
     }
 
-    descurtir(): void {
-        this._curtidas--;
-    }
-
     visualizar(): void {
         console.log(`ID: ${this._id}`)
         console.log(`Texto: ${this._texto}`)
         console.log(`Legenda: ${this.legenda}`)
         console.log(`Data: ${this._data.getDate()}/${this._data.getMonth() + 1}/${this._data.getFullYear()}`)
         console.log(`Autor: ${this._autor.nome}`)
+        console.log(`Curtidas: ${this._curtidas} curtidas\n`)
     }
 
     get id(): number {
@@ -76,15 +73,21 @@ class Post {
 
 
 class RedeSocial {
-    private Armazena_dados: repo_post = new repo_post()
+    private posts: repo_post = new repo_post()
+    usuarios_da_rede: Usuario[] = []
     constructor(
         private _id: number,
         private _nome: string,
+        private _url: string,
         private _id_post: number = 1
     ) { }
 
     get nome(): string {
         return this._nome
+    }
+
+    get url(): string {
+        return this._url
     }
 
     get id(): number {
@@ -93,6 +96,45 @@ class RedeSocial {
 
     get id_post(): number {
         return this._id_post
+    }
+
+    consultar_usuario(id: number): Usuario {
+        let usuario !: Usuario
+        for (let i = 0; i < this.usuarios_da_rede.length; i++) {
+            if (this.usuarios_da_rede[i].id == id) {
+                usuario = this.usuarios_da_rede[i]
+            }
+        }
+        if (usuario == null) { throw new usuario_inexistente("Usuario não encontrado!!") }
+        return usuario
+    }
+
+    private consultar_index_usuario(id: number): number {
+        let index: number = -1
+        for (let i = 0; i < this.usuarios_da_rede.length; i++) {
+            if (this.usuarios_da_rede[i].id == id) {
+                index = i
+            }
+        }
+        if (index == -1) { throw new usuario_inexistente("Usuario não encontrado!!") }
+        return index
+    }
+
+    inserir_usuario(usuario: Usuario): void {
+        this.usuarios_da_rede.push(usuario)
+    }
+
+    qtd_posts(): number {
+        let qtd: number = this.posts.qtd()
+        return qtd
+    }
+
+    excluir_usuario(id: number): void {
+        let index: number = this.consultar_index_usuario(id)
+        for (let i = index; i < this.usuarios_da_rede.length; i++) {
+            this.usuarios_da_rede[i] = this.usuarios_da_rede[i + 1]
+        }
+        this.usuarios_da_rede.pop()
     }
 
     ValidarValor(valor: number): void {
@@ -105,48 +147,55 @@ class RedeSocial {
         this.ValidarValor(id)
         let post!: Post
         try {
-            post = this.Armazena_dados.consultar(id).Valor
+            post = this.posts.consultar(id).Valor
         } catch (e: any) {
             if (e instanceof Valor_inexistente) {
                 throw new post_inexistente("Erro, o post não foi encontrado!!")
             }
-
         }
         return post
     }
 
+    ids_posts(): number[] {
+        let ids: number[] = []
+        let no_aux: No<Post> | null = this.posts.inicio
+
+        while (no_aux) {
+            ids.push(no_aux.Valor.id)
+        }
+
+        if (ids.length == 0) { throw new post_inexistente("Não há posts cadastrados!!") }
+        return ids
+    }
+
 
     inserir_post(post: Post): void {
-        this.Armazena_dados.inserir(post)
+        this.posts.inserir(post)
         this._id_post++;
     }
 
     alterar_post(novo_post: Post): void {
         this.ValidarValor(novo_post.id)
-        this.Armazena_dados.alterar(novo_post)
+        this.posts.alterar(novo_post)
     }
 
     excluir_post(id: number): void {
         this.ValidarValor(id)
-        this.Armazena_dados.excluir(id)
+        this.posts.excluir(id)
     }
 
-    curtir_post(id: number): void {
-        this.ValidarValor(id)
-        this.Armazena_dados.curtir(id)
+    curtir_post(id_post: number): void {
+        this.ValidarValor(id_post)
+        this.posts.curtir(id_post)
     }
 
-    descurtir_post(id: number): void {
-        this.ValidarValor(id)
-        this.Armazena_dados.descurtir(id)
-    }
 
     visualizar_post(id: number): void {
         this.ValidarValor(id)
-        this.Armazena_dados.visualizar(id)
+        this.posts.visualizar(id)
     }
 
     visualizar_todos_os_posts(): void {
-        this.Armazena_dados.visualizar_todos()
+        this.posts.visualizar_todos()
     }
 }
